78519325c8457927bf3d492bf5538bd998f25d53
aadarshjha <jha.aadarsh@gmail.com>
2020-12-26 00:01:32 -0600

    cards are mocked

--- front-end/src/App.svelte
+++ front-end/src/App.svelte
@@ -2,29 +2,20 @@
 	// materialize ui. 
 	import '../node_modules/materialize-css/dist/css/materialize.min.css';
 	import '../node_modules/materialize-css/dist/js/materialize.min.js';
-	import { Router, Link, Route } from "svelte-routing";
+	import '../node_modules/animate.css/animate.min.css'
+
 	import Title from './layout/Title.svelte'
 	import TableDesc from './layout/TableDesc.svelte'
 	import UserCards from './layout/UserCards.svelte'
-	import About from './Pages/About.svelte'
-	import '../node_modules/animate.css/animate.min.css'
 </script>
-
-
 <style>
 
 </style>
-
-
-<!-- <Router> -->
 <Title />
 <TableDesc />
+<UserCards />
 
-	<!-- <div class = "container">
-		<Route path = "/about" component = {About} />
-	</div> -->
 
-<!-- </Router> -->
 
 
 
--- front-end/src/layout/UserCards.svelte
+++ front-end/src/layout/UserCards.svelte
@@ -1,6 +1,92 @@
 <script>
+    // fetch data here, but acting like a db with some mock data. 
+    let mockData = [
+        {
+            name: "aadarsh"
+        },
+        {
+            name: "sayd"
+        },
+        {
+            // will do 
+        }
+    ]
 </script>
 
 <style>
-
+.makeSmall {
+    
+}
+.smallGray {
+    background-color: #CAF0F8;  
+    width: 300px;
+    height: 300px;
+}
+.wrapper {
+    background-color: red; 
+    display: flex;
+    justify-content: center;
+    /* align-items: center; */
+}
 </style>
+
+<div class = "wrapper">
+    <div class="row">
+        <div class="col">
+          <div class="card-panel smallGray">
+            <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+            </span>
+        </div>
+        <div class="card-panel smallGray">
+            <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+            </span>
+          </div>
+          <div class="card-panel smallGray">
+            <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+            </span>
+          </div>
+
+        </div>
+        <div class="col ">
+            <div class="card-panel smallGray">
+              <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+              </span>
+            </div>
+            <div class="card-panel smallGray">
+                <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+              </div>
+              <div class="card-panel smallGray">
+                <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+              </div>
+       
+          </div>
+          <div class="col ">
+            <div class="card-panel smallGray">
+              <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+              </span>
+            </div>
+            <div class="card-panel smallGray">
+                <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+              </div>
+              <div class="card-panel smallGray">
+                <span class="black-text">I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                </span>
+              </div>
+
+       
+          </div>
+    </div>
+    
+</div>
