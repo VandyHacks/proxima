f6ce32ae3b56d29caf151c7428554a406621cf43
aadarshjha <jha.aadarsh@gmail.com>
2020-12-26 00:39:12 -0600

    finalized card designs

--- front-end/src/layout/UserCards.svelte
+++ front-end/src/layout/UserCards.svelte
@@ -2,13 +2,40 @@
     // fetch data here, but acting like a db with some mock data. 
     let mockData = [
         {
-            name: "aadarsh"
+            name: "Aadarsh Jha",
+            shouldInterview: "", 
+            status: ""
         },
         {
-            name: "sayd"
+            name: "Saydolimkhon Agzamkhodjaev",
+            shouldInterview: "",
+            status: ""
+
         },
         {
-            name: "sam"
+            name: "Samuel Lee",
+            shouldInterview: "", 
+            status: ""
+        }, 
+        {
+            name: "John Doe",
+            shouldInterview: "", 
+            status: ""
+        }, 
+        {
+            name: "Surya Thalli",
+            shouldInterview: "", 
+            status: ""
+        }, 
+        {
+            name: "Gabriel Ting",
+            shouldInterview: "", 
+            status: ""
+        }, 
+        {
+            name: "Ananya Gondi",
+            shouldInterview: "", 
+            status: ""
         }
     ]
 </script>
@@ -22,7 +49,39 @@
     }
 
     .card-panel {
-        width: 250px; 
+        width: 250px;
+        min-height: 270px;
+        padding-top: 0px;
+
+    }
+
+    #intervierName {
+        margin-top: 15px;
+    }
+    .icons {
+        display: flex; 
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+        /* position: relative; */
+
+    }
+    .emojis p {
+        font-size: 30px;
+        padding: 0;
+        margin: 0;
+        margin-top: 5px
+    }
+    .color {
+        background-color: rgb(197, 235, 248); 
+    }
+    .interviewNotes a {
+        font-size: 20px;
+    }
+    .space {
+        height: 10px;
     }
 </style>
 
@@ -30,20 +89,38 @@
 <div class = "justifyCards">
 
     <!-- svelte foreach -->
-    <div class="card-panel teal">
+    {#each mockData as inter}
+    <div class="card-panel black">
         <center>
-            <span class="white-text">Aadarsh Jha</span>
+            <h5 id = "intervierName" class="white-text">{inter.name}</h5>
+            <div class="space"></div>
             <div class = "interviewStatus">
                 <i class="white-text">Interview Potential Status</i>
                 <div class = "icons">
-                    <div>
-                        
+                    <div class = "emojis">
+                        <p>&#9989;</p>  
                     </div>
-                    <div>
-
+                    <div class = "emojis">
+                        <p>&#10060;</p>
                     </div>
-                    <div>
-
+                    <div class = "emojis">
+                        <p>&#128339;</p>
+                    </div>
+                </div>
+            </div>
+            <div class = "space"></div>
+            <div class = "interviewNotes">
+                <i><a href="">Interview Notes</a></i>
+            </div>
+            <div class = "space"></div>
+            <div class = "status">
+                <i class="white-text">Accept Or Reject</i>
+                <div class = "icons">
+                    <div class = "emojis">
+                        <p>&#9989;</p>  
+                    </div>
+                    <div class = "emojis">
+                        <p>&#10060;</p>
                     </div>
                 </div>
             </div>
@@ -51,5 +128,6 @@
         </center>
         
     </div>  
+    {/each}
 </div>
 
