6c8b22caec40dc31fc893be9f0b004009e405daf
aadarshjha <jha.aadarsh@gmail.com>
2020-12-27 01:13:48 -0600

    finished typeform rewrite

--- front-end/src/layout/TypeForm.svelte
+++ front-end/src/layout/TypeForm.svelte
@@ -1,4 +1,5 @@
 <style>
+    
     .divider {
         width: 400px;
         margin-top: 30px;
@@ -43,39 +44,69 @@
     h6 {
         text-align: justify; 
     }
+
+    .collapsible {
+        width: 40%;
+    }
+    .collapsible-body {
+        text-overflow:ellipsis; 
+        /* background-color: red;  */
+        background-color: rgb(249, 249, 249); 
+    }
+    .collapsible-body span {
+        /* background-color:red;  */
+    }
 </style>
 
 <script>
     import typeForm from "../typeformMockData.json";
-    console.log(typeForm)
+    // console.log(typeForm)
     let result = [];
-    let result2 = []; 
-    let result3 = []; 
-    let result4 = []; 
-    let counter = 0; 
+    // let result2 = []; 
+    // let result3 = []; 
+    // let result4 = []; 
+    // let counter = 0; 
     for (let i in typeForm) {
-        counter = counter + 1; 
-        if (counter < 7) {
-            result.push([i, typeForm[i]]);
+        result.push([i, typeForm[i]]);
+        // counter = counter + 1; 
+        // if (counter < 7) {
+        //     result.push([i, typeForm[i]]);
             
-        } else if (counter >= 7 && counter <= 9) {
-            result2.push([i, typeForm[i]]); 
-        } else if (counter >= 12 && counter <= 15) {
-            result3.push([i, typeForm[i]]); 
-        } else {
-            result4.push([i, typeForm[i]]); 
-        }
+        // } else if (counter >= 7 && counter <= 9) {
+        //     result2.push([i, typeForm[i]]); 
+        // } else if (counter >= 12 && counter <= 15) {
+        //     result3.push([i, typeForm[i]]); 
+        // } else {
+        //     result4.push([i, typeForm[i]]); 
+        // }
         
     }
 
     // let result2 = [1,2,3,4]; 
     // console.log(result); 
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
 </script>
 
 <center>
     <div class="divider"></div>
     <h5>Typeform Data</h5>
 </center>
+
+<div class = "wrapper">
+    <ul class="collapsible">
+        {#each result as query}
+            <li class = "active">
+            <div class="collapsible-header">{query[0]}</div>
+            <div class="collapsible-body"><span>{query[1]}</span></div>
+            </li>
+        {/each}
+      </ul>
+</div>
+
+<!-- 
 <div class= "wrapper">
     <div class = "generalInformation">
         {#each result as query}
@@ -129,4 +160,4 @@
         </div>
     </div>
 </div>
-
+ -->
