dd2d606d82c03ce04301324080511f351b33c018
saydus <saydus07@gmail.com>
2020-12-27 12:42:48 -0600

    Changed context type to router context

--- server/routes/controllers/notesController.ts
+++ server/routes/controllers/notesController.ts
@@ -1,7 +1,7 @@
 import { Application, CommitteeChoice, QuestionNote, Note, Question } from "../../database/models.ts"
-import { Context } from "../../deps.ts"
+import { RouterContext } from "../../deps.ts"
 
-const questionCreate = async(ctx: Context) => {
+const questionCreate = async(ctx: RouterContext) => {
     // populate application
 
     // populate committee
