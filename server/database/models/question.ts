594d60ca593331401ffc02e77dba9a671709f603
saydus <saydus07@gmail.com>
2020-12-26 20:45:09 -0600

    Question model created

---
+++ server/database/models/question.ts
@@ -0,0 +1,13 @@
+import { Model, DataTypes } from "../../deps.ts";
+
+class Question extends Model {
+    static table = 'questions';
+    static timestamps = true;
+    static fields = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true
+        },
+        content: DataTypes.TEXT
+    };
+}
