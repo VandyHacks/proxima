389a5e5c6433a4bfbbd9ee1d326517c7e1ea3770
aadarshjha <jha.aadarsh@gmail.com>
2020-12-27 00:19:40 -0600

    dynamic data loaded

--- front-end/src/layout/TypeForm.svelte
+++ front-end/src/layout/TypeForm.svelte
@@ -3,20 +3,64 @@
         width: 400px;
         margin-top: 30px;
     }
-
+    .generalInformation {
+        background-color: red;
+        width: 80%;
+    }
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
 
 <script>
+    import typeForm from "../typeformMockData.json";
+    console.log(typeForm)
+    let result = [];
+    for (let i in typeForm) {
+        if(i == "whyCommittee") {
+            break; 
+        }
+        result.push([i, typeForm[i]]);
+    }
 
-
+    let result2 = [1,2,3,4]; 
+    // console.log(result); 
 </script>
 
 <center>
     <div class="divider"></div>
     <h5>Typeform Data</h5>
 </center>
-
-<div class = "">
-
+<div class= "wrapper">
+    <div class = "generalInformation">
+    
+        <div>
+            {#each result as query}
+            <h4>
+                {query[0]}
+            </h4>
+            <p>
+                {query[1]}
+            </p>
+            {/each}
+        </div>
+        
+    
+    </div>
+    
+    <div class = "essayResponses">
+    
+    </div>
+    
+    <div class = "misc">
+    
+    </div>
+    
+    <div class = "socials">
+    
+    </div>
 </div>
 
