11ca4371193b06188fa55a5ba901212aaf2e97d2
aadarshjha <jha.aadarsh@gmail.com>
2020-12-27 22:28:55 -0600

    introduction

--- front-end/src/layout/InterviewForm.svelte
+++ front-end/src/layout/InterviewForm.svelte
@@ -89,6 +89,17 @@
             </div>
         {/if}
         {/each}
+        <div class = "section">
+            <h5>Intro Blurb
+            </h5>
+            <p>Thank you for applying to VandyHacks!
+                <br>
+                <br>
+                As a quick overview for how this interview will go, we'll ask you a a couple of questions just to get to know your work habits, then we'll go into a couple questions specific to the committees you indicated you were interested in on your application. If you have any questions, we'll leave a couple of minutes at the end and we can try to answer them as best as possible. Of course, if you don't have any questions, don't feel pressured. We won't count them as part of your application. 
+                <br>
+                <br>
+                We will be taking notes on our laptops to refer back to while you answer.</p>
+        </div>
     </div>
    
     <div>
