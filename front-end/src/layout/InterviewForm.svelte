e8ea312afb753e249a93563c31c607d377ffedd9
aadarshjha <jha.aadarsh@gmail.com>
2020-12-27 21:44:41 -0600

    did more static google form stuff

---
+++ front-end/src/layout/InterviewForm.svelte
@@ -0,0 +1,63 @@
+<style>
+    img {
+        width: 100%;
+        height: auto;
+    }
+    .googleForm {
+        display: flex; 
+        flex-direction: column;
+        justify-content: center;
+        /* background-color: green;  */
+        align-items: center;
+        margin-top: 20px;
+    }
+    .section {
+        /* background-color: red;  */
+        display: flex; 
+        flex-direction: column;
+        padding: 0; 
+        background-color: lightblue;
+        margin-bottom: 20px;
+        width: 50%;
+        padding-left: 10px;
+        padding-right: 10px; 
+        padding-bottom: 10px;
+        border-radius: 5px;
+    }
+    .vandy p {
+        font-size: 20px;
+        margin: 10px; 
+    }
+    .introduction {
+        display: flex; 
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+
+<script>
+    // import googleForm from "../googleForm.json"
+    // console.log(googleForm)
+
+</script>
+
+
+<div class = "googleForm">
+    <div class = "introduction">
+        <div class = "intro section">
+            <h5>VIII Interview Form</h5>
+            <p>Interviewer form for recruitment season 8 in 2021</p>
+        </div>
+        <div class = "vandy section">
+            <p>&#127881; &#128512;</p>
+            <img src="res/vandy.png" alt = "VandyHacks Board!"/>
+        </div>
+        <!-- some dynamic stuff -->
+        
+    </div>
+   
+    <div>
+
+    </div>
+</div>
--- front-end/src/layout/PageThree.svelte
+++ front-end/src/layout/PageThree.svelte
@@ -1,6 +1,7 @@
 <script>
     import Title from "./Title.svelte";
     import { Link } from "svelte-routing";
+    import InterviwForm from "./InterviewForm.svelte"
 
     // passing user name from the previous page 
     // and then obtaining full context.
@@ -9,11 +10,11 @@
 </script>
 
 <Title/>
-    <center>   
-        <Link to = "/">Go Home</Link>
-        <h5>Interview Notes For {name}</h5>
-    </center>
-    <!-- <TypeForm /> 
-    <GoogleForm />  -->
+<center>   
+    <Link to = "/">Go Home</Link>
+    <h5>Interview Form For {name}</h5>
+</center>
+
+<InterviwForm />
 
 
