a2b79dad1c9bf19e5f13d66145c861ef3213188e
saydus <saydus07@gmail.com>
2020-12-29 11:50:57 -0600

    Seperated requests for readibility

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -4,6 +4,10 @@ import * as notesController from "./controllers/notesController.ts"
 
 const router = new Router();
 
+/**
+ * POST REQUESTS 
+ */
+
 // Application submission
 router.post('/typeform/submit', applicationController.parseTypeForm);
 
@@ -13,12 +17,16 @@ router.post('/application/status', applicationController.updateStatus);
 // Add questions to interview question list
 router.post('/interview/questions/add', notesController.questionCreate);
 
+
+/**
+ * GET REQUESTS
+ */
+
 // Fetch all questions 
 router.get('/interview/questions/list', notesController.getAllQuestions);
 
 // Fetch questions for a specific applicant
 router.get('/interview/questions', notesController.getQuestionsForApplicant);
-
 
 
 
