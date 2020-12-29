e04dbb3c569065b6970136864f5b3313d741e5b0
aadarshjha <jha.aadarsh@gmail.com>
2020-12-29 15:12:27 -0600

    folder restructure from meeting

--- front-end/src/Pages/PageOne.svelte
+++ front-end/src/Pages/PageOne.svelte
@@ -1,10 +1,10 @@
 <script>
-    import Title from "./Title.svelte";
-    import TableDesc from "./TableDesc.svelte";
-    import UserCards from "./UserCards.svelte"; 
+    import Title from "../layout/Title.svelte";
+    import TableDesc from "../layout/TableDesc.svelte";
+    import UserCards from "../layout/UserCards.svelte"; 
     import { Router, Link, Route } from "svelte-routing";
-    import PageTwo from "./PageTwo.svelte"
-    import PageThree from "./PageThree.svelte"
+    import PageTwo from "../layout/PageTwo.svelte"
+    import PageThree from "../layout/PageThree.svelte"
     import { component_subscribe } from "svelte/internal";
     window.scrollTo({top: 0})
 </script>
