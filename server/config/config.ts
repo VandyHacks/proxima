a08e5e230e3ce2066536e03e97595087c7877ce7
saydus <saydus07@gmail.com>
2020-12-29 19:42:01 -0600

    Automated email sending

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,14 +1,23 @@
-let config: any = {};
+let config: {database?: any, smtp?: any} = {};
 
 // Set the database configuration
 if (!Deno.env.get('TEST_ENVIRONMENT')) {
   config.database = {
-    hostname: Deno.env.get("HOSTNAME"),
-    database: Deno.env.get("DATABASENAME"),
-    username: Deno.env.get("DBUSER"),
-    password: Deno.env.get("DBPASSWORD"),
+    hostname: Deno.env.get("DB_HOSTNAME"),
+    database: Deno.env.get("DB_NAME"),
+    username: Deno.env.get("DB_USER"),
+    password: Deno.env.get("DB_PASSWORD"),
     port: 5432
   };
+
+  config.smtp = {
+    hostname: Deno.env.get("SMTP_HOSTNAME"),
+    port: Deno.env.get("SMTP_PORT"),
+    username: Deno.env.get("SMTP_USER"),
+    password: Deno.env.get("SMTP_PASSWORD"),
+  };
+
+  config.smtp = Deno.env.get("EMAIL_ADDRESS")
 }
 
 export { config }; 
