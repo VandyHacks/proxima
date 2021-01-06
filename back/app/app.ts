bd9b4d0bdbcb9747c073946ea549c5d9c48a2d06
saydus <saydus07@gmail.com>
2021-01-06 13:16:45 -0600

    Started the node koa project

---
+++ back/app/app.ts
@@ -0,0 +1,35 @@
+import * as Koa from 'koa';
+import * as koaBody from 'koa-body';
+import * as cors from '@koa/cors';
+
+const serve = require('koa-static');
+const koaValidator = require('koa-async-validator');
+const koaSwagger = require('koa2-swagger-ui');
+const koaBunyanLogger = require('koa-bunyan-logger');
+
+import { config } from './config';
+import { routes } from './routes';
+import { logger } from './logger';
+
+const app = new Koa();
+
+app.use(koaBody());
+app.use(koaValidator());
+app.use(cors());
+app.use(koaBunyanLogger(logger));
+app.use(koaBunyanLogger.requestLogger());
+app.use(koaBunyanLogger.timeContext());
+app.use(routes);
+app.use(serve('public'));
+app.use(
+  koaSwagger({
+    routePrefix: '/swagger',
+    swaggerOptions: {
+      url: '/swagger.yml',
+    },
+  })
+);
+
+export const server = app.listen(config.port);
+
+console.log(`Server running on port ${config.port}`);
