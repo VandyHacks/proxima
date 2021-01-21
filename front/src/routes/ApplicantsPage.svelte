41ebd018581353e3ef7638729ce3da26cd50e4a6
aileen <44736622+wua3@users.noreply.github.com>
2021-01-21 02:09:32 -0600

    Prettier fix (for accepted committee click)

--- front/src/routes/ApplicantsPage.svelte
+++ front/src/routes/ApplicantsPage.svelte
@@ -156,7 +156,7 @@
         {capitalizeFirstLetter(replaceUnderscores(cell.value))}
       {:else if cell.key === 'committees'}
         {#if row.status === ApplicationStatus.ACCEPTED}
-          <Tag 
+          <Tag
             type="green"
             on:click={() => updateCommitteeSelection(row.committee_accepted)}>
             {capitalizeFirstLetter(row.committee_accepted)}
