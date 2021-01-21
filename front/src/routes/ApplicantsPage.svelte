d6ac7b07767528625f63d97345c3767166ea8cb1
aileen <44736622+wua3@users.noreply.github.com>
2021-01-21 01:04:53 -0600

    Run prettier lint

--- front/src/routes/ApplicantsPage.svelte
+++ front/src/routes/ApplicantsPage.svelte
@@ -81,27 +81,34 @@
   });
 
   let selectedCommittee = null;
-  
-  const passesCommitteeFilters = (rows: any[], selectedCommittee: CommitteeType) => {
-    if (selectedCommittee==null) {return rows;}
-    return rows.filter(function(row) {
+
+  const passesCommitteeFilters = (
+    rows: any[],
+    selectedCommittee: CommitteeType
+  ) => {
+    if (selectedCommittee == null) {
+      return rows;
+    }
+    return rows.filter(function (row) {
       let commExists = false;
-      row.committees.forEach(function(commObj: { committee: CommitteeType; }) {
-        if (commObj.committee == selectedCommittee) {commExists = true;}
+      row.committees.forEach(function (commObj: { committee: CommitteeType }) {
+        if (commObj.committee == selectedCommittee) {
+          commExists = true;
+        }
       });
       return commExists;
     });
-  }
+  };
 
   const updateCommitteeSelection = (committee: CommitteeType) => {
     selectedCommittee = committee;
     return true;
-  }
+  };
 
   const getCommitteeSelectionText = (selectedCommittee: CommitteeType) => {
-    let text = (selectedCommittee!=null) ? selectedCommittee.toString() : "all";
-    return " (" + text + ")";
-  }
+    let text = selectedCommittee != null ? selectedCommittee.toString() : 'all';
+    return ' (' + text + ')';
+  };
 </script>
 
 <svelte:window on:click={click} />
@@ -119,11 +126,12 @@
         <ToolbarSearch bind:value={searchTerm} />
       </ToolbarContent>
     </Toolbar> -->
-    <Button 
-      kind="secondary" 
-      disabled={selectedCommittee==null ? true : false}
+    <Button
+      kind="secondary"
+      disabled={selectedCommittee == null ? true : false}
       on:click={() => updateCommitteeSelection(null)}>
-      Clear committee filter: {selectedCommittee==null ? "none" : selectedCommittee}
+      Clear committee filter:
+      {selectedCommittee == null ? 'none' : selectedCommittee}
     </Button>
     <span slot="cell-header" let:header>
       {#if header.key === 'committees'}
@@ -153,7 +161,9 @@
           </Tag>
         {:else}
           {#each cell.value as { committee }}
-            <Tag type={colors[committee]} on:click={() => updateCommitteeSelection(committee)}>
+            <Tag
+              type={colors[committee]}
+              on:click={() => updateCommitteeSelection(committee)}>
               {capitalizeFirstLetter(committee)}
             </Tag>
           {/each}
