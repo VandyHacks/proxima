69bed8bc73a9adb7a7f6dc81e4826f6e52e71e7f
semenodp <dmitry.p.semenov@vanderbilt.edu>
2021-02-07 15:20:16 -0600

    Remove commented out part

--- backend/src/database.connection.ts
+++ backend/src/database.connection.ts
@@ -1,24 +1,6 @@
 import 'reflect-metadata';
 import { createConnection, Connection, ConnectionOptions } from 'typeorm';
 
-// const database =
-//   process.env.NODE_ENV === 'test'
-//     ? 'test'
-//     : process.env.POSTGRES_DB || 'postgres';
-
-// const connectionOpts: ConnectionOptions = {
-//   type: 'postgres',
-//   host: process.env.POSTGRES_HOST || 'localhost',
-//   port: Number(process.env.POSTGRES_PORT) || 5432,
-//   username: process.env.POSTGRES_USER || 'postgres',
-//   password: process.env.POSTGRES_PASSWORD || '',
-//   database,
-//   entities: [`${__dirname}/entities/*.ts`],
-//   migrations: [`${__dirname}/migrations/*.ts`],
-//   // synchronize: true,
-//   logging: false
-// };
-
 const connection: Promise<Connection> = createConnection();
 
 export default connection;
