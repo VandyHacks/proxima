fd6d63b53a79940b5dad7cec5d1ef3ebb0daa1e8
semenodp <dmitry.p.semenov@vanderbilt.edu>
2021-02-22 21:43:35 -0600

    Add generate jwt function

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import * as Router from 'koa-router';
-import * as passport from 'passport';
+import * as passport from 'koa-passport';
 
 const router = new Router();
 
@@ -7,11 +7,14 @@ router.get('/auth/slack', async ctx => {
   return passport.authorize('Slack')(ctx);
 });
 
-router.get('/auth/slack/callback', async ctx => {
-  return passport.authenticate('Slack', {
-    successRedirect: '/',
-    failureRedirect: '/'
-  })(ctx);
-});
+const generateJwt = async ctx => {
+  console.log(ctx.req.user);
+  // generate jwt token here like in tutorial
+  // redirect
+  // set cookie
+  ctx.redirect('/');
+};
+
+router.get('/auth/slack/callback', passport.authenticate('Slack'), generateJwt);
 
 export default router;
